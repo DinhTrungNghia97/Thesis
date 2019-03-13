8cce2c9e813136c9f611d2b1b0b80d48cc7edc70
DinhTrungNghia97 <trungnghiadinh97@gmail.com>
2019-03-13 23:52:46 +0700

    Add Centre Node

---
+++ Energia/Centre_Node/Centre_Node.ino
@@ -0,0 +1,38 @@
+//Author: Dinh Trung Nghia
+//TivaC Centre Node
+String inputString2 = ""; 
+boolean stringComplete2 = false; // State Machine
+
+void setup() {
+  //Setup Serial
+  Serial.begin(19200);
+  Serial2.begin(19200);
+  // declare 200 bytes for inputString2:
+  inputString2.reserve(200);
+  Serial.println("Tiva C Centre Node Start");
+  delay(200);
+}
+
+void loop() {
+  // print string when get new line
+  if (stringComplete2) {
+    Serial.println(inputString2); 
+  // Empty string
+    inputString2 = "";
+    stringComplete2 = false;
+  }
+}
+
+
+void serialEvent2(){  //Uart Interrupt
+    while (Serial2.available()) {
+    // Get new byte
+    char inChar2 = (char)Serial2.read(); 
+    // Add this byte to String
+    inputString2 += inChar2;
+    // Set flag
+    if (inChar2 == '\n') {
+    stringComplete2 = true;
+    } 
+    }
+}
